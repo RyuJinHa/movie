c0e9de3c4090b5102f14ff6981b84d49108f3aad
RyuJinHa <bb_jinha98@naver.com>
2018-12-11 14:48:28 +0900

    마지막!

--- main.c
+++ main.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[]) {
 	while (fgetc(fp)!=EOF) /* read name, country, runtime and score*/ 
 	{	
 		//generate a movie info instance(mvInfo) with function mv_genMvInfo()
-		fscanf(fp, "%s %s %d %f",&name,&country,&runTime,&score);
+		fscanf(fp,"%s %s %d %f",&name,&country,&runTime,&score);
 		mvInfo=mv_genMvInfo(name, score, runTime, country);
 		list_addTail(mvInfo, list);//list 만들기  
 		
@@ -47,18 +47,18 @@ int main(int argc, char *argv[]) {
 	while(exit_flag == 0) 
 	{
 		//2.1 print menu message and get input option
-		printf("-------------------Menu-------------------\n");
+		printf("---------------------Menu---------------------\n");
 		printf("1. print all the movies in the list \n2. search for specific country movies \n3. search for specific runtime movies \n");
 		printf("4. search for specific score movies \n5. exit\n");
-		printf("-------------------Menu-------------------\n");
+		printf("---------------------Menu---------------------\n");
 		printf("select an option : ");
 		scanf("%d",&option);
-		
+	
 		switch(option)
 		{
 			case 1: //print all the movies
 				printf("\nprinting all the movies in the list.....\n");
-				printf("-------------------------------------------\n");
+				printf("----------------------------------------------\n");
 				
 				ndPtr = list;
 				while ( list_isEndNode(ndPtr) == 0) /* repeat until the ndPtr points to the end node */
@@ -80,11 +80,10 @@ int main(int argc, char *argv[]) {
 				ndPtr = list;
 				printf("--input a country to find: ");
 				scanf("%s",&inputCountry);//사용자로부터 원하는 값 받기  
-				printf("-------------------------------------------\n");	
+				printf("----------------------------------------------\n");	
 				while ( list_isEndNode(ndPtr) == 0)/* repeat until the ndPtr points to the end node */
 				{
 					//2.3.2 print a movie data : use functions of movie.c and linkedList.c
-				
 					//ndPtr = the next node of the ndPtr;
 					ndPtr=list_getNextNd(ndPtr);
 					//get object of ndPtr to mvInfo void pointer
@@ -93,7 +92,7 @@ int main(int argc, char *argv[]) {
 					if(strcmp(mv_getCountry(mvInfo), inputCountry)==0)
 					{
 						mv_print(mvInfo);
-						printf("-------------------------------------------\n");
+						printf("----------------------------------------------\n");
 						count++;//영화 개수 세기  
 					}
 					//then print the contents of the mvInfo
@@ -113,7 +112,7 @@ int main(int argc, char *argv[]) {
 				ndPtr = list;
 				printf("--input a runtime to find: ");
 				scanf("%d",&inputRuntime);
-				printf("-------------------------------------------\n");	
+				printf("----------------------------------------------\n");	
 				while ( list_isEndNode(ndPtr) == 0/* repeat until the ndPtr points to the end node */)
 				{
 					//2.4.2 print a movie data : use functions of movie.c and linkedList.c
@@ -126,7 +125,7 @@ int main(int argc, char *argv[]) {
 					if(mv_getRunTime(mvInfo)>inputRuntime)
 					{
 						mv_print(mvInfo);
-						printf("-------------------------------------------\n");
+						printf("----------------------------------------------\n");
 						count++;
 					}
 					//then print the contents of the mvInfo
@@ -146,7 +145,7 @@ int main(int argc, char *argv[]) {
 				ndPtr = list;
 				printf("--input a score to find: ");
 				scanf("%f",&inputScore);
-				printf("-------------------------------------------\n");
+				printf("----------------------------------------------\n");
 				
 				while (list_isEndNode(ndPtr) == 0/* repeat until the ndPtr points to the end node */)
 				{
@@ -160,7 +159,7 @@ int main(int argc, char *argv[]) {
 					if(mv_getScore(mvInfo)>inputScore)
 					{
 						mv_print(mvInfo);
-						printf("-------------------------------------------\n");
+						printf("----------------------------------------------\n");
 						count++;
 					}
 					//then print the contents of the mvInfo
@@ -182,6 +181,6 @@ int main(int argc, char *argv[]) {
 			
 		}
 	}
-	
+	free(ndPtr);//메모리 정리  
 	return 0;
 }
--- movie.c
+++ movie.c
@@ -18,13 +18,13 @@ void* mv_genMvInfo(char *name, float score, int runTime, char* country)
 	//allocate memory and set the member variables
 	mvPtr=(movInfo_t*)malloc(sizeof(movInfo_t));
 	
-	strcpy(mvPtr->name,name);
-	mvPtr->name[200]=*name;
-	mvPtr->score=score;
-	mvPtr->runTime=runTime;	
+	strcpy(mvPtr->name,name);//문자열 복사  
+	mvPtr->name[200]=*name;//name 설정  
+	mvPtr->score=score;//score 설정  
+	mvPtr->runTime=runTime;	//runTime 설정  
 	
-	strcpy(mvPtr->madeIn,country);
-	mvPtr->madeIn[10]=*country;
+	strcpy(mvPtr->madeIn,country);//문자열 복사  
+	mvPtr->madeIn[10]=*country;//country 설정  
 	
 	return (void*)mvPtr;
 }
@@ -47,29 +47,28 @@ void mv_print(void* obj)
 //return the score value from the input instance of movInfo_t structure
 float mv_getScore(void* obj)
 {
-		movInfo_t* mvPtr = (movInfo_t*)obj;
-		return  mvPtr->score;
+	movInfo_t* mvPtr = (movInfo_t*)obj;
+	return  mvPtr->score;
 }
 
 //return the runtime value from the input instance of movInfo_t structure
 int mv_getRunTime(void* obj)
 {
-		movInfo_t* mvPtr = (movInfo_t*)obj;
-		return mvPtr->runTime;
+	movInfo_t* mvPtr = (movInfo_t*)obj;
+	return mvPtr->runTime;
 }
 
 //return the name string pointer from the input instance of movInfo_t structure
 char* mv_getName(void* obj)
 {
-		movInfo_t* mvPtr = (movInfo_t*)obj;
-		return (void*)mvPtr->name;
+	movInfo_t* mvPtr = (movInfo_t*)obj;
+	return (void*)mvPtr->name;
 }
 
 //return the country string pointer from the input instance of movInfo_t structure
 char* mv_getCountry(void* obj)
 {
 	movInfo_t* mvPtr = (movInfo_t*)obj;
-	
 	return (void*)mvPtr->madeIn;
 }
 
